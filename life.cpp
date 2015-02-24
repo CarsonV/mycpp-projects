83ff0f7f7a10f2c438b2240e60c4f8969750927f
CarsonV <CarsonV@users.noreply.github.com>
2015-02-24 12:10:50 -0600

    Update life.cpp

--- life.cpp
+++ life.cpp
@@ -11,6 +11,7 @@ const int YSIZE = 75;
 
 void printWorld(char[][YSIZE]);
 void populate(char[][YSIZE]);
+void generation(char[][YSIZE]);
 int _tmain(int argc, _TCHAR* argv[])
 {
 
@@ -23,12 +24,12 @@ int _tmain(int argc, _TCHAR* argv[])
 	//print out array
 	printWorld(world);
 	//calculate live and dead cell moves
-
+	generation(world);
 	//print out new array
 	system("pause > NULL");
 	return 0;
 }
-void populate(char world[XSIZE][YSIZE])
+void populate(char world[XSIZE][YSIZE]) //inital seed for the world array
 {
 	char live = 178;
 
@@ -44,7 +45,7 @@ void populate(char world[XSIZE][YSIZE])
 	}
 	
 }
-void printWorld(char world[XSIZE][YSIZE])
+void printWorld(char world[XSIZE][YSIZE]) //prints out the world array
 {
 	for (int y = 0; y < 75; y++)
 	{
@@ -53,4 +54,34 @@ void printWorld(char world[XSIZE][YSIZE])
 			cout << world[x][y];
 		}
 	}
+}
+void generation(char world[XSIZE][YSIZE])
+{
+	int next = 0;
+	for (int y = 0; y < 75; y++)
+	{
+		for (int x = 0; x < 22; x++)
+		{
+			//if (y > 1 && x > 1 )
+			{
+				if ((world[x - 1][y]) ==  178) // search directly to the left for life
+					next++;
+				if (world[x - 1][y + 1] == 178)// up left
+					next++;
+				if (world[x][y + 1] == 178) // directly above
+					next++;
+				if (world[x + 1][y + 1] == 178) // up right
+					next++;
+				if (world[x + 1][y] == 178) // directly right
+					next++;
+				if (world[x + 1][y - 1] == 178) // down right
+					next++;
+				if (world[x][y - 1] == 178) // directly below
+					next++;
+				if (world[x - 1][y - 1] == 178) // down left
+					next++;
+			}
+		}
+	}
+	cout << next;
 }
